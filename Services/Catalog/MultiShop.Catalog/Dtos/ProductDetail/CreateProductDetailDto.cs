ce588a16da042e6500ec12b93469ceead51b8ed2
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-04-02 02:49:41 +0300

    Add project files.

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetail/CreateProductDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetail
+{
+    public class CreateProductDetailDto
+    {
+        public string Description { get; set; }
+        public decimal Info { get; set; }
+        public string ProductId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductDetail/UpdateProductDetailDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiShop.Catalog.Dtos.ProductDetail
+{
+    public class UpdateProductDetailDto
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public decimal Info { get; set; }
+        public string ProductId { get; set; }
+    }
+}
