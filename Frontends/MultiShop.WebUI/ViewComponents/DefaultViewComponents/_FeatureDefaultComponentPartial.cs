0c87cc72c7cfb0bd48a5b95f300d1dc16a286717
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-05-14 01:10:26 +0300

    Some view bugs fixed. Feature admin panel completed.

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _FeatureDefaultComponentPartial:ViewComponent
     {
 
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public _FeatureDefaultComponentPartial(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Feature");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData.Result);
+                return View(values);
+            }
+
             return View();
         }
     }
