0589e10000b111e38d73ea93c14c3ab42d0ebcc5
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-05-14 00:43:48 +0300

    Feature client side dto's and controller added.

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/FeatureController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureDtos;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/Feature")]
+    public class FeatureController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public FeatureController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.PageHeader = "Öne Çıkan Alan İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Öne Çıkan Alan";
+            ViewBag.v3 = "Öne Çıkan Alan Listesi";
+
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Feature");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData.Result);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("CreateFeature")]
+        public IActionResult CreateFeature()
+        {
+            ViewBag.PageHeader = "Öne Çıkan Alan İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Öne Çıkan Alan";
+            ViewBag.v3 = "Öne Çıkan Alan Ekleme";
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateFeature")]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var client = _clientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("https://localhost:7070/api/Feature", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+        [Route("DeleteFeature/{id}")]
+        public async Task<IActionResult> DeleteFeature(string id)
+        {
+            var client = _clientFactory.CreateClient();
+
+            var responseMessage = await client.DeleteAsync("https://localhost:7070/api/Feature/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+
+            return View();
+        }
+
+
+        [Route("UpdateFeature/{id}")]
+        public async Task<IActionResult> UpdateFeature(string id)
+        {
+            ViewBag.PageHeader = "Öne Çıkan Alan İşlemleri";
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Öne Çıkan Alan";
+            ViewBag.v3 = "Öne Çıkan Alan Güncelleme";
+
+            var client = _clientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Feature/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateFeatureDto>(jsonData.Result);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UpdateFeature/{id}")]
+        public async Task<IActionResult> UpdateFeature(string id, UpdateFeatureDto updateFeatureDto)
+        {
+            var client = _clientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/Feature/", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Feature", new { area = "Admin" });
+            }
+
+            return View();
+        }
+    }
+}
