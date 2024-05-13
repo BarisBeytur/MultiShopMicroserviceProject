b44f7df52d38b99bf6277bca1b9786f1ec8030ec
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-05-14 00:21:17 +0300

    Homepage special offers area developments.

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
-    public class _SpecialOfferComponentPartial:ViewComponent
+    public class _SpecialOfferComponentPartial : ViewComponent
     {
 
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public _SpecialOfferComponentPartial(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/SpecialOffer");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData.Result);
+                return View(values);
+            }
+
             return View();
         }
     }
