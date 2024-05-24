f63d1263abe71efed87e6ba3f5a8fb3447efd086
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-05-24 20:06:34 +0300

    Product Detail 1

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailFeatureComponentPartial.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailFeatureComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _clientFactory;
+
+        public _ProductDetailFeatureComponentPartial(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string id)
+        {
+
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/product/" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData.Result);
+                return View(values);
+            }
+
             return View();
         }
+
     }
 }
