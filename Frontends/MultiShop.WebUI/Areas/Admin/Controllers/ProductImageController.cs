2ac0bad43691758981c87084dae40a13f559ec27
Baris Beytur <Baris.Beytur@d-teknoloji.com.tr>
2024-05-25 23:02:03 +0300

    Fix: Ürün fotoğrafları kısmı güncellendi.

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
@@ -1,11 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.ProductImageDtos;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
     public class ProductImageController : Controller
     {
+
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ProductImageController(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        [HttpGet]
+        [Route("ProductImageDetail/{id}")]
         public async Task<IActionResult> ProductImageDetail(string id)
         {
+            var client = _clientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/ProductImage/ProductImagesByProductId?id=" + id);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdProductImageDto>(jsonData.Result);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("ProductImageDetail/{id}")]
+        public async Task<IActionResult> ProductImageDetail(GetByIdProductImageDto getByIdProductImageDto)
+        {
+            var client = _clientFactory.CreateClient();
+
+            var jsonData = JsonConvert.SerializeObject(getByIdProductImageDto);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync("https://localhost:7070/api/ProductImage/", content);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" });
+            }
+
             return View();
         }
     }
